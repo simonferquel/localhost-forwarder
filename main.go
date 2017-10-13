65f66ec50b381f18da4b2e60306965f75ad8bdae
Simon Ferquel <simon.ferquel@docker.com>
2017-10-13 09:14:43 +0200

    Add events support
    Signed-off-by: Simon Ferquel <simon.ferquel@docker.com>

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"time"
+
+	"github.com/docker/docker/api/types"
 )
 
 func main() {
@@ -13,14 +16,37 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Can't create reconciler: %s\n", err.Error())
 		return
 	}
+
+	ch := make(chan struct{})
+	go func() {
+		for {
+			ch <- struct{}{}
+			time.Sleep(20 * time.Second)
+		}
+	}()
+	go func() {
+		for {
+			evs, errs := r.docker.Events(context.Background(), types.EventsOptions{})
+			for {
+				select {
+				case <-errs:
+					break // re-launch event loop
+				case msg := <-evs:
+					if msg.Type == "container" &&
+						(msg.Action == "kill" || msg.Action == "pause" || msg.Action == "restart" || msg.Action == "start" || msg.Action == "stop" || msg.Action == "unpause" || msg.Action == "update") {
+						ch <- struct{}{}
+					}
+				}
+			}
+		}
+	}()
 	for {
+		<-ch
 		err = r.reconcile()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Reconciliation failed: %s\n", err.Error())
 		} else {
 			fmt.Printf("Reconciliation succeeded\n")
 		}
-
-		time.Sleep(5 * time.Second)
 	}
 }
